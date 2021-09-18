b34982d612af3e9d03c960b70c782dd8a1f1544a
Vinay Panchal <65890217+vinaynpp@users.noreply.github.com>
2021-09-18 18:18:13 +0530

    ready for deploy

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ app.before_first_request(initialization)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(threaded=True, port=port, debug=True, host="0.0.0.0")
+    app.run(threaded=True, port=port, debug=True)
